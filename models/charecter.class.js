e454924ca8f0e9282282d0727d492bcfaf1da2a5
julian <julian.weishaar@gmx.de>
2024-03-05 17:56:41 +0100

    add animate character

--- models/charecter.class.js
+++ models/charecter.class.js
@@ -15,10 +15,10 @@ class Character extends MovableObject {
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
-    this.animae();
+    this.animate();
   }
 
-  animae() {
+  animate() {
     setInterval(() => {
       let i = this.currentImage % this.IMAGES_WALKING.length;
       let path = this.IMAGES_WALKING[i];
