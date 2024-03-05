8eeb3c7bb05a88fa5079dbb233b431af3492cfad
julian <julian.weishaar@gmx.de>
2024-03-05 17:56:09 +0100

    add animate character

--- models/charecter.class.js
+++ models/charecter.class.js
@@ -2,8 +2,29 @@ class Character extends MovableObject {
   y = 180;
   height = 250;
   width = 150;
+  IMAGES_WALKING = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImage = 0;
+
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.IMAGES_WALKING);
+    this.animae();
+  }
+
+  animae() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 90);
   }
 
   jump() {}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,5 +1,6 @@
 class MovableObject {
   img;
+  imageCache = {};
   x = 120;
   y = 280;
   height = 150;
@@ -8,6 +9,15 @@ class MovableObject {
   loadImage(path){
     this.img = new Image(); // this.img ist das gleiche wie document.getElementByID('image')
     this.img.src = path
+  }
+
+  loadImages(arr){
+    arr.forEach((path) => {
+      let img = new Image()
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+   
   }
 
   moveRight() {
